13f14733f796f9711e1c3779c5fe59793ab1fba2
X-Chiku-X <jesuspetitpimentel@gmail.com>
2024-07-05 19:27:59 -0400

    Login_Recuperar

--- vista/login.php
+++ vista/login.php
@@ -62,7 +62,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     
-                    <h4 class="modal-title">Mensaje de Error</h4>
+                    <h4 class="modal-title">Nuevo Mensaje</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
