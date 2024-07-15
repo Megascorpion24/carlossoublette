54220fc5b217d90b7e81277e302ec7f6e0c2c92a
Megascorpion24 <santiagocasamayor14@gmail.com>
2024-07-15 19:50:07 -0400

    fix: ambio de token a controlador

--- controlador/inscripciones.php
+++ controlador/inscripciones.php
@@ -1,5 +1,21 @@
 <?php 
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable("../carlossoublette/");
+$dotenv->load();
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
+$key = $_ENV['JWT_SECRET_KEY'];
+
+if(isset($_COOKIE['token'])){
+	$decoded = JWT::decode($_COOKIE['token'], new Key($key, 'HS256'));
+	$nivel = $decoded->resultado->user;
+    $rol =  $decoded->resultado->rol;
+    $nivel1 = $decoded->resultado->permisos;
+    $name = $decoded->resultado->name;
+} else {
+	header('location:index.php');
+}
 
 require 'vendor/autoload.php';
  //archivo de conecion para que funciuone 
